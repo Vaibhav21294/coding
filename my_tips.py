9a83d8ec9b29a2cc0e7d488c5bb4ada04e97447d
Vaibhav <vaibhavnagvekar4@gmail.com>
2026-07-06 21:43:00 -0700

    Update tip

--- my_tips.py
+++ my_tips.py
@@ -1,9 +1,11 @@
 """
 My tips
 
-1. Dry run data structure questions before implementing code
+1. Always do brute force first. Atleast I have one solution by now
 
-2. When you see code like this:
+2. Dry run data structure questions before implementing code
+
+3. When you see code like this:
 for i in range(n):
     for j in range(i + 1, n):
     
@@ -16,6 +18,4 @@ The number of unique pairs in a collection of n items is:
 n(n - 1) / 2
 
 and that simplifies to O(n²).
-
-3. Always do brute force first. Atleast I have one solution by now
 """
