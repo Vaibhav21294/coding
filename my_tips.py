e46f241dc2c0e7d7124880d3e947b634ecf822eb
Vaibhav <vaibhavnagvekar4@gmail.com>
2026-07-08 15:56:54 -0700

    Update tip

--- my_tips.py
+++ my_tips.py
@@ -39,4 +39,45 @@ class Solution:
             key = "".join(sorted(str))
             groups[key].append(str)
         return list(groups.values())
+
+7. Problem solving:
+Brute Force
+Optimal solution
+Time complexity
+Space complexity
+
+8. In Python, a set is a built-in data type used to store an unordered collection of unique items.
+# Creating a set with elements
+fruits = {"apple", "banana", "cherry", "apple"} 
+print(fruits)  # Output: {'banana', 'cherry', 'apple'} (Duplicates are removed!)
+
+# Creating an empty set
+# Note: {} creates an empty dictionary, so you must use set()
+empty_set = set() 
+
+# Converting a list to a set (useful for removing duplicates)
+numbers_list = [1, 2, 2, 3, 4, 4]
+unique_numbers = set(numbers_list)
+print(unique_numbers)  # Output: {1, 2, 3, 4}
+
+my_set = {1, 2, 3}
+
+# Add a single item
+my_set.add(4) # {1, 2, 3, 4}
+
+# Add multiple items from any iterable
+my_set.update([4, 5, 6]) # {1, 2, 3, 4, 5, 6}
+
+# Remove an item (Raises KeyError if the item doesn't exist)
+my_set.remove(3) 
+
+# Discard an item (Does NOT raise an error if the item is missing)
+my_set.discard(10) 
+
+# Remove and return an arbitrary element
+popped_item = my_set.pop() 
+
+# Clear all items
+my_set.clear() # set()
+
 """
