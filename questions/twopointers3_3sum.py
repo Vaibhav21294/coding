d0dbd188a85cc5eaf8737bbcd72d40da38198a76
Vaibhav <vaibhavnagvekar4@gmail.com>
2026-07-21 21:26:48 -0700

    3Sum

---
+++ questions/twopointers3_3sum.py
@@ -0,0 +1,14 @@
+"""
+3Sum is one of the most important interview problems because it combines sorting with the two-pointer pattern.
+
+Problem
+
+Given an array, return all unique triplets whose sum is 0.
+
+Example:
+
+nums = [-1,0,1,2,-1,-4]
+
+Output:
+[[-1,-1,2],[-1,0,1]]
+"""
